9e72a0a943db94bcabc4841402b8789cf4b77e94
bengro <benjamin.groehbiel@gmail.com>
2013-04-24 16:54:20 +0200

    separating between different headline generation classes. added test folder

---
+++ test/general/PosTagging.java
@@ -0,0 +1,14 @@
+package general;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PosTagging {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
